e366236ca083d4be13a4dd8c23fd156581c32b7e
VanBudur <111625941+VanBudur@users.noreply.github.com>
2024-04-27 17:54:33 +0400

    f

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -1,7 +1,13 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import './styles/resets.css'
+import './styles/base.css'
+import './styles/footer.css'
+import './styles/form.css'
+import './styles/header.css'
 
-console.log(checkForName);
+import { checkForName } from './js/nameChecker'
+import { handleSubmit,showResult } from './js/formHandler'
+
+console.log(checkForName,showResult);
 
 alert("I EXIST")
 console.log("CHANGE!!");
--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,4 +1,5 @@
-function handleSubmit(event) {
+function showResult(event) {
+    console.log('gg')
     event.preventDefault()
 
     // check what text was put into the form field
@@ -9,8 +10,52 @@ function handleSubmit(event) {
     fetch('http://localhost:8080/test')
     .then(res => res.json())
     .then(function(res) {
+        console.log(res.message)
         document.getElementById('results').innerHTML = res.message
     })
 }
+import { checkForURL } from './urlChecker';
+const form = document.getElementById('urlForm');
+form.addEventListener('submit', handleSubmit);
 
-export { handleSubmit }
+function handleSubmit(event) {
+    event.preventDefault();
+    // Get the URL from the input field
+    const formText = document.getElementById('name').value;
+    // Check if the URL is valid
+    if (checkForURL(formText)) {
+    // If the URL is valid, send it to the server
+    postData('/api', { url: formText })
+    .then(response => {
+    console.log('Server response:', response);
+    // Handle the response from the server and update dom
+    document.getElementById('results').innerHTML =
+    
+    response.score_tag;
+    })
+    .catch(error => {
+    console.error('Error:', error);
+    // Handle errors
+    });
+    } else {
+    alert('Invalid URL');
+    console.log('Invalid URL');
+    // Handle invalid URL
+    }
+    }
+    async function postData(url = '', data = {}) {
+        try {
+        const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+        });
+        return response.json();
+        } catch (error) {
+        throw error;
+        }
+        }
+
+export { handleSubmit,showResult }
